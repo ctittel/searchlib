ebab4b5e960e292c8ce9de918caca7975ed6e236
Christoph Tittel <chti02@dfki.de>
2021-07-29 16:00:40 +0200

    fix and improve tabu search

--- searchlib/tabu.py
+++ searchlib/tabu.py
@@ -12,7 +12,7 @@ def tabu(
 ):
     """
     SolutionType can be anything that can be anything that can be handeled by the corresponding callback functions.
-    FitnessType (the type returned by the fitness function) must support comparison (>, ==, etc.). Greater fitness values are better
+    FitnessType (the type returned by the fitness function) must support comparison with >. Greater fitness values are better
 
     Inputs:
     - initial_solution: State to begin search from
@@ -22,20 +22,30 @@ def tabu(
     - max_tabu_list_len: How long the tabu list may get before entries are removed
     """
     best = initial_solution
+    best_fitness = get_fitness(best)
     best_candidate = initial_solution
     
     tabu_list = [initial_solution]
 
-    while not stopping_condition(initial_solution, get_fitness(initial_solution)):
+    while not stopping_condition(best, best_fitness):
         neighbors = list(get_neighbors(best_candidate))
         if len(neighbors) == 0:
             raise Exception(f"get_neighbors({best_candidate}) returned no objects! Need neighbors")
+        
         best_candidate = neighbors[0]
-        for candidate in neighbors:
-            if (candidate not in tabu_list) and (get_fitness(candidate) > get_fitness(best_candidate)):
-                best_candidate = candidate
-        if get_fitness(best_candidate) > get_fitness(best):
+        best_candidate_fitness = get_fitness(best_candidate)
+
+        for candidate in neighbors[1:]:
+            if candidate not in tabu_list:
+                candidate_fitness = get_fitness(candidate)
+                if candidate_fitness > best_candidate_fitness:
+                    best_candidate = candidate
+                    best_candidate_fitness = candidate_fitness
+
+        if best_candidate_fitness > best_fitness:
             best = best_candidate
+            best_fitness = best_candidate_fitness
+
         tabu_list.append(best_candidate)
         if len(tabu_list) > max_tabu_list_len:
             tabu_list = tabu_list[1:]
