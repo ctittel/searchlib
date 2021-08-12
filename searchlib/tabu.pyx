b6773155e6a80dbfaa2d822962b83ce7299583c0
Christoph Tittel <chti02@dfki.de>
2021-08-12 15:17:17 +0200

    rename tabu search callback functions

--- searchlib/tabu.pyx
+++ searchlib/tabu.pyx
@@ -7,9 +7,9 @@ SolutionType = Any
 FitnessType = Any
 def tabu(
     initial_solution: SolutionType,
-    get_neighbors: Callable[[SolutionType], Iterable[SolutionType]],
-    get_fitness: Callable[[SolutionType], FitnessType],
-    stopping_condition: Callable[[SolutionType, FitnessType], bool],
+    get_neighbors_fn: Callable[[SolutionType], Iterable[SolutionType]],
+    get_fitness_fn: Callable[[SolutionType], FitnessType],
+    stopping_fn: Callable[[SolutionType, FitnessType, int], bool],
     max_tabu_list_len: int = 100,
     include_best_fitness: bool = False
 ):
@@ -19,43 +19,43 @@ def tabu(
 
     Inputs:
     - initial_solution: State to begin search from
-    - get_neighbors: Function get_neighbors(solution) -> Iterable(SolutionType): Returns other solutions in the neighborhood of the given solution 
-    - get_fitness: Function get_fitness(solution) -> cost: Fitness of the given solution (a CostType)
-    - stopping_condition: Function stopping_condition(best_solution, best_solution_fitness) -> bool: Called every iteration; if it returns true, the search will be stopped. Users can implement this as they want. The condition may for example use a time or iteration limit, threshold on fitness improvement etc.
+    - get_neighbors_fn: Function get_neighbors_fn(solution) -> Iterable(SolutionType): Returns other solutions in the neighborhood of the given solution 
+    - get_fitness_fn: Function get_fitness_fn(solution) -> cost: Fitness of the given solution (a CostType)
+    - stopping_fn: Function stopping_fn(best_solution, best_solution_fitness) -> bool: Called every iteration; if it returns true, the search will be stopped. Users can implement this as they want. The condition may for example use a time or iteration limit, threshold on fitness improvement etc.
     - max_tabu_list_len: How long the tabu list may get before entries are removed
     - include_total_cost: If true, returns a tuple (best solution, best solution's fitness); otherwise, only the best solution is returned
     """
-    return _tabu(initial_solution, get_neighbors, get_fitness, stopping_condition, max_tabu_list_len, include_best_fitness)
+    return _tabu(initial_solution, get_neighbors_fn, get_fitness_fn, stopping_fn, max_tabu_list_len, include_best_fitness)
 
 cdef object _tabu(
     initial_solution,
-    get_neighbors,
-    get_fitness,
-    stopping_condition,
+    get_neighbors_fn,
+    get_fitness_fn,
+    stopping_fn,
     max_tabu_list_len: int,
     include_best_fitness: bool
 ):
     best = initial_solution
-    best_fitness = get_fitness(best)
+    best_fitness = get_fitness_fn(best)
     best_candidate = initial_solution
     
     tabu_list = [initial_solution]
     i: int = 0
-    while not stopping_condition(best_fitness=best_fitness, iteration=i):
-        neighbors = get_neighbors(best_candidate)
+    while not stopping_fn(best=best, fitness=best_fitness, i=i):
+        neighbors = get_neighbors_fn(best_candidate)
         if len(neighbors) == 0:
-            raise Exception(f"get_neighbors({best_candidate}) returned no objects! Need neighbors")
+            raise Exception(f"get_neighbors_fn({best_candidate}) returned no objects! Need neighbors")
         neighbors = [x for x in neighbors if x not in tabu_list]
         if len(neighbors) == 0:
             return best
 
         assert best_candidate != neighbors[0]
         best_candidate = neighbors.pop(0)
-        best_candidate_fitness = get_fitness(best_candidate)
+        best_candidate_fitness = get_fitness_fn(best_candidate)
 
         for candidate in neighbors:
             assert candidate != best_candidate
-            candidate_fitness = get_fitness(candidate)
+            candidate_fitness = get_fitness_fn(candidate)
             if candidate_fitness > best_candidate_fitness:
                 best_candidate = candidate
                 best_candidate_fitness = candidate_fitness
