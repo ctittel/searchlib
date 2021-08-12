6d423072464ddf0cbaaaabf3688af2f4ca498304
Christoph Tittel <chti02@dfki.de>
2021-08-12 10:58:51 +0200

    refactor A*

--- searchlib/astar.pyx
+++ searchlib/astar.pyx
@@ -38,7 +38,7 @@ cdef class MyMinHeap:
         self.l = []
         self.d = {}
 
-    cdef push(self, state: object, cost: object):
+    cdef cython.void push(self, state: object, cost: object):
         if state in self.d:
             self.d[state].active=False
         node = Node(state, cost)
@@ -46,7 +46,7 @@ cdef class MyMinHeap:
         self.l.append(node)
         self.l = sorted(self.l)
 
-    cdef cython.object pop(self):
+    cdef object pop(self):
         while len(self.l):
             node = self.l.pop(0)
             if node.active == True:
@@ -97,10 +97,13 @@ def astar(initial_state: StateType,
         def f(g_cost, state):
             return g_cost
 
-    return _astar(initial_state, initial_cost, is_goal, get_actions, get_state, get_cost, f, graph_search, include_states, include_total_cost)
+    best, best_cost = _astar(initial_state, initial_cost, is_goal, get_actions, get_state, get_cost, f, graph_search, include_states)
+    if include_total_cost:
+        return (best, best_cost)
+    else:
+        return best
 
-@cython.cfunc
-def _astar(initial_state,
+cdef _astar(initial_state,
           initial_cost,
           is_goal,
           get_actions,
@@ -109,7 +112,6 @@ def _astar(initial_state,
           f,
           graph_search: bool,
           include_states: bool,
-          include_total_cost: bool,
           ):
     open_set: MyMinHeap = MyMinHeap()
     open_set.push(initial_state, f(initial_cost, initial_state))
@@ -119,7 +121,8 @@ def _astar(initial_state,
     while True:
         current_state = open_set.pop()
         if current_state == None:
-            raise Exception("No path found!")
+            # No path found
+            return (None, None)
 
         g_current: object = g_scores[current_state]
         if is_goal(current_state):
@@ -127,10 +130,7 @@ def _astar(initial_state,
             if include_states == False:
                 _states, actions = zip(*path)
                 path: list = actions[:-1]
-            if include_total_cost:
-                return (path, g_current)
-            else:
-                return path
+            return (path, g_current)
         for action in get_actions(current_state):
             g_cost: object = get_cost(current_state, action)
             next_state: object = get_state(current_state, action)
